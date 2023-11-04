d255001303ad7c46025066f3cbeb75e62c84f031
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2023-11-04 10:57:02 +0200

    Add computer guess logic

--- main.rb
+++ main.rb
@@ -4,8 +4,15 @@ require_relative './user_interface'
 colors = ["red", "orange", "yellow", "green", "blue", "violet"]
 game = MastermindGameLogic.new(colors)
 
-12.times do
-  game.play_round
+if UserInterface.get_user_choice == 1
+  game.generate_user_code(UserInterface.get_user_code(colors))
+  12.times do
+  game.play_round1
+  end
+elsif UserInterface.get_user_choice == 2
+  12.times do
+    game.play_round1
+    end
 end
 
 puts "Your number of tries are over! The correct answer was #{game.secret_code}"
