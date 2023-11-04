32be1027a8ff914f55032173ae1f636ebc71d9e6
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2023-11-04 16:58:15 +0200

    Fix get_user_code

--- main.rb
+++ main.rb
@@ -4,15 +4,22 @@ require_relative './user_interface'
 colors = ["red", "orange", "yellow", "green", "blue", "violet"]
 game = MastermindGameLogic.new(colors)
 
-if UserInterface.get_user_choice == 1
-  game.generate_user_code(UserInterface.get_user_code(colors))
-  12.times do
-  game.play_round1
-  end
-elsif UserInterface.get_user_choice == 2
+ans = UserInterface.get_user_choice.to_i
+if ans == 1
+  game.generate_user_code
   12.times do
     game.play_round1
+    if game.won == true
+      break
     end
+  end
+elsif ans == 2
+  12.times do
+    game.play_round2
+    if game.won == true
+      break
+    end
+  end
 end
 
 puts "Your number of tries are over! The correct answer was #{game.secret_code}"
