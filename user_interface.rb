5c945d773f66c9530c5e58486bffe7126c968e9b
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2023-11-29 11:46:31 +0200

    Add function for creating code, playing again

--- user_interface.rb
+++ user_interface.rb
@@ -1,11 +1,32 @@
-
+require File.dirname(__FILE__) + '\colored-master\lib\colored.rb'
 class UserInterface
     def self.get_user_guess(colors)
-        puts "Type 4 colors from the following list in the order you wish to guess, separated by a space:"
+        puts "----Type the number of 4 colors from the following list in the order you wish to guess, separated by a space:"
         view(colors)
         a = gets.chomp
         array = a.split(' ')
         array
+    end
+
+    def self.get_user_code(colors)
+        puts "----Type the number of 4 colors from the following list that will form your secret code separated by a space:"
+        view(colors)
+        a = gets.chomp
+        array = a.split(' ')
+        new_arr = array.map { | item | item.to_i }
+        new_arr
+    end
+
+    def self.get_user_choice
+        puts "----Do you want to create the secret code or guess it? (1/2)"
+        a = gets.chomp
+        a.to_i
+    end
+
+    def self.get_user_again
+        puts "----Do you want to play again? (y/n)"
+        a = gets.chomp
+        a
     end
 
     def self.display_guess_feedback(correct, exists, attempts)
@@ -19,8 +40,21 @@ class UserInterface
     end
 
     def self.view(colors)
-        colors.each do |x| 
-            puts x
+        for i in 0..6
+            x = colors[i].to_i
+            if x == 1
+                print "   1   ".white_on_red
+            elsif x == 2
+                print "   2   ".white_on_yellow
+            elsif x == 3
+                print "   3   ".white_on_green
+            elsif x == 4
+                print "   4   ".white_on_blue
+            elsif x == 5
+                print "   5   ".white_on_cyan
+            elsif x == 6
+                print "   6   ".white_on_magenta
+            end
         end
     end
 end
