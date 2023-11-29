fdf5c56f3f2e5df7be2990fd5d759db1655224b5
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2023-11-29 11:44:00 +0200

    Create gui.rb

---
+++ gui.rb
@@ -0,0 +1,26 @@
+
+class GUI
+    def self.logo
+        puts "
+        ███╗   ███╗ █████╗ ███████╗████████╗███████╗██████╗ ███╗   ███╗██╗███╗   ██╗██████╗ 
+        ████╗ ████║██╔══██╗██╔════╝╚══██╔══╝██╔════╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗
+        ██╔████╔██║███████║███████╗   ██║   █████╗  ██████╔╝██╔████╔██║██║██╔██╗ ██║██║  ██║
+        ██║╚██╔╝██║██╔══██║╚════██║   ██║   ██╔══╝  ██╔══██╗██║╚██╔╝██║██║██║╚██╗██║██║  ██║
+        ██║ ╚═╝ ██║██║  ██║███████║   ██║   ███████╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██████╔╝
+        ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═════╝ 
+                                                                                            
+        "
+        puts "                                                              WRITTEN IN RUBY BY DIANA "
+    end
+
+    def self.rules
+        puts
+        puts
+        puts "--------RULES----------"
+        puts "Mastermind is a game where you guess a computer-generated sequence of 4 out of 6 colors in a maximum of 12 attempts. Alternatively, you can be the one to create the code and let the computer guess."                
+        puts "After every guess, you are given feedback on it and how it compares to the secret code."
+    end
+end
+
+# GUI.logo
+# GUI.rules
