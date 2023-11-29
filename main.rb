28a33a5fd88ef2d6d0b3f55e71d81bc4580e2246
obamas-lastname <131595909+obamas-lastname@users.noreply.github.com>
2023-11-29 11:44:46 +0200

    Change from color names to numbers main.rb

--- main.rb
+++ main.rb
@@ -1,11 +1,36 @@
 require_relative './mastermind_game_logic'
 require_relative './user_interface'
+require_relative './gui'
 
-colors = ["red", "orange", "yellow", "green", "blue", "violet"]
-game = MastermindGameLogic.new(colors)
+GUI.logo
+GUI.rules
+colors = [1, 2, 3, 4, 5, 6]
 
-12.times do
-  game.play_round
+loop do
+  game = MastermindGameLogic.new(colors)
+  ans = UserInterface.get_user_choice
+  if ans == 1
+    game.generate_user_code
+    puts game.secret_code
+    12.times do
+      game.play_round1
+      if game.won == true
+        break
+      end
+    end
+  elsif ans == 2
+    12.times do
+      game.play_round2
+      if game.won == true
+        break
+      end
+    end
+  end
+
+  puts "Game over! The correct answer was:"
+  UserInterface.view(game.secret_code)
+  if UserInterface.get_user_again != 'y'
+    break
+  end
 end
 
-puts "Your number of tries are over! The correct answer was #{game.secret_code}"
